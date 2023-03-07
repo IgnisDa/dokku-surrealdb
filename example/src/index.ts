17bd6ce8018400c579b21e40c53cb5386abe14bc
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-03-07 14:01:43 +0530

    fix(example): use correct namespace and db

--- example/src/index.ts
+++ example/src/index.ts
@@ -14,6 +14,7 @@ router.get("/", async (ctx, next) => {
 		user: process.env.SURREAL_USER,
 		pass: process.env.SURREAL_PASS,
 	});
+	await db.use("test", "test");
 	await db.create("person", {
 		title: "Founder & CEO",
 		name: { first: "Tobie", last: "Morgan Hitchcock" },
